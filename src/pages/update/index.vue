2cf5ab89bb513d676a731346814a21bba9b2f6b6
Ryanro <1149990423@qq.com>
2018-05-25 23:12:43 +0800

    today's uodate

---
+++ src/pages/update/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page">
+    <div class="weui-uploader__bd">
+      <div class="weui-uploader__files" id="uploaderFiles">
+        <block  v-for="item in files" :key="index">
+          <div class="weui-uploader__file" @click="predivImage" :id="item" style="margin-top: 10rpx">
+            <image class="weui-uploader__img" :src="item" mode="aspectFill" />
+          </div>
+          <image @click="removeImage" src="/static/icons/delete.png" style="position: absolute;height: 30rpx;width: 30rpx;margin-left: -30rpx;"/>
+        </block>
+      </div>
+      <div v-if="!files.length" class="weui-uploader__input-box" style="margin-top: 10rpx">
+        <div class="weui-uploader__input" @click="chooseImage"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        files: []
+      }
+    },
+    methods: {
+      chooseImage(e) {
+        var _this = this;
+        wx.chooseImage({
+          count: 1,
+          sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+          sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+          success: function (res) {
+            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+            _this.files = _this.files.concat(res.tempFilePaths)
+          },
+          fail: function () {
+            console.log('fail');
+          },
+          complete: function () {
+            console.log('commplete');
+          }
+        })
+      },
+      removeImage() {
+        this.files = [];
+      },
+      predivImage(e) {
+        console.log(e);
+        wx.previewImage({
+          current: e.currentTarget.id, // 当前显示图片的http链接
+          urls: this.files // 需要预览的图片http链接列表
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
--- src/router/today.js
+++
@@ -1,7 +0,0 @@
-let express = require('express')
-let app = express()
-
-app.post('/src/pages/today/photo',function(req,res){
-    
-    res.send('hello world')
-})

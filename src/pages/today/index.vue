8934f359574fddfe67582658e5d6afbbe013c5cb
马勇杰 <516225899@qq.com>
2018-05-30 12:15:55 +0800

    add onload to get data at today index.vue

--- src/pages/today/index.vue
+++ src/pages/today/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="userbackground">
+<div>
+  <div class="show" v-if="istoday===1">
+    <div class="show"><img src="../../../static/picture/shiLiTu.jpg" class="showBody showImage"></div>
+    <div class="show"><p class="showBody showText">今天真是个好日子</p></div>
+    <div class="show"><p class="showBody showAddress">广州市白云区广源大道中151号</p></div>
+  </div>
+  <div class="userbackground" v-if="istoday===0">
     <form report-submit="true" @submit="formSubmit">
       <button form-type="submit">
         <div class="updiv"></div>
@@ -17,9 +23,10 @@
       <button form-type="submit">
         <div class="bottomdiv"></div>
       </button>
-
     </form>
   </div>
+</div>
+  
 </template>
 
 <script>
@@ -30,8 +37,98 @@
       ripple
     },
     data() {
-      return {}
+      return {
+        time:'',
+        image:'',
+        text:'',
+        address:'',
+        isStorage:0,//当isStorage为0时，即缓存不存在，否则缓存存在，从缓存中读取数据
+        istoday:1,//用于测试，当istoday为1的时候，即用户已上传图片
+        openID:'',
+        wxcode:'',//以下三个数据都用于返回给服务器以获得openid
+        encryptedData:'',
+        iv:'',
+      }
     },
+    onLoad:function(){
+      // 页面初始化
+      //首先应该获取全局变量istoday的布尔值
+      //然后查看缓存，如果有读取缓存信息，否则向服务器获取信息
+      wx.getStorage({
+          key: 'key',
+          success: function(res) {
+              this.isStorage=1
+              console.log(res.data)
+          },
+          fail:function(res){
+              this.isStorage=0
+              console.log('fail to get storage')
+          }
+      })
+      if(this.isStorage===0){
+          wx.login({
+            success: function (res) {
+              //微信js_code
+              this.wxcode=res.code
+              console.log('wxcode='+this.wxcode)
+              //获取用户信息
+              wx.getUserInfo({
+                  success: function (res) {
+                    //获取用户敏感数据密文和偏移向量
+                    console.log(res.encryptedData)
+                    console.log(res.iv)
+                    this.encryptedData=res.encryptedData
+                    this.iv=res.iv
+                  },
+                  fail:function(){
+                    console.log('fail to get UserInfo')
+                  }
+              })
+            }
+          })
+          wx.request({
+              url: '../today/getopenID', //仅为示例，并非真实的接口地址
+              data: {
+                  wxcode: this.wxcode ,
+                  encryptedData: this.encryptedData,
+                  iv:this.iv
+              },
+              header: {
+                  'content-type': 'application/json' // 默认值
+              },
+              success: function(res) {
+                console.log('success get user openID')
+                console.log(res.data)
+                //获得服务器传来的openID
+                //接下来是将服务器获得的data信息绑定到image,text,address
+              },
+              fail:function(){
+                console.log('fail to get openID')
+              }
+          })
+          wx.request({
+              url: '../today/getData', //仅为示例，并非真实的接口地址
+              data: {
+                  openID: this.openID ,
+              },
+              header: {
+                  'content-type': 'application/json' // 默认值
+              },
+              success: function(res) {
+                  //通过openID获得用户上传的信息
+                  console.log('success get user publish data')
+                  console.log(res.data)
+                  //然后将数据保存到image,text,address中
+                  this.image=res.image
+                  this.text=res.text
+                  this.address=res.address
+              },
+              fail:function(){
+                console.log('fail to get user data')
+              }
+          })
+        }
+      },
     methods: {
       navigate(){
         wx.navigateTo({
@@ -40,7 +137,7 @@
             console.log("success")
           }
         })
-      }
+      },
     }
   }
 </script>
@@ -86,6 +183,34 @@
     text-align: center;
     line-height: 1.5;
     -webkit-tap-highlight-color: transparent;
+  }
+  .show{
+    margin: 0 auto;
+    padding: 0;
+    border: 0;
+    width: 100%;
+  }
+  .showBody{
+      margin-left: 10%;
+      margin-right: 10%;
+      margin-top:5%;
+      width: 80%;
+  }
+  .showImage{
+      margin-left: 10%;
+      margin-right: 10%;
+      margin-top:5%;
+      width: 80%;
+  }
+  .showText{
+      text-align: center;
+      font-size: 28rpx;
+      color: #000000;
+  }
+  .showAddress{
+      text-align: center;
+      font-size: 28rpx;
+      color: #000000;
   }
 
 </style>

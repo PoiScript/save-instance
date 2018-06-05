93bce07429353c7c2ff40e962e6a9891934ae0c9
PoiScript <poiscript@gmail.com>
2018-06-05 09:58:01 +0800

    refactor: update util

--- src/util.js
+++ src/util.js
@@ -31,6 +31,10 @@ export const request = (path, method, data) => {
 /** promisify wx.uploadFile api */
 export const upload = (path, filePath, formData) => {
   wx.showNavigationBarLoading();
+  wx.showToast({
+    icon: 'loading',
+    title: '正在上传...',
+  });
   return new Promise((resolve, reject) => {
     wx.uploadFile({
       formData,
@@ -40,15 +44,26 @@ export const upload = (path, filePath, formData) => {
       header: { 'content-type': 'multipart/form-data' },
       success: res => {
         if (res.statusCode !== 200) {
+          wx.showModal({
+            content: '上传失败!',
+            title: '提示',
+            showCancel: false,
+          });
           reject('Request Failed with status code' + res.statusCode);
         } else {
           resolve(res.data);
         }
       },
       fail: err => {
+        wx.showModal({
+          content: '上传失败!',
+          title: '提示',
+          showCancel: false,
+        });
         reject('Network Failed: ' + err);
       },
       complete: () => {
+        wx.hideToast();
         wx.hideNavigationBarLoading();
       },
     });
@@ -58,19 +73,34 @@ export const upload = (path, filePath, formData) => {
 /** promisify wx.login api */
 export const login = () => {
   wx.showNavigationBarLoading();
+  wx.showToast({
+    icon: 'loading',
+    title: '正在登录...',
+  });
   return new Promise((resolve, reject) => {
     wx.login({
       success: res => {
         if (res.code) {
           resolve(res.code);
         } else {
+          wx.showModal({
+            content: '登录失败!',
+            title: '提示',
+            showCancel: false,
+          });
           reject('Login Failed');
         }
       },
       fail: () => {
+        wx.showModal({
+          content: '登录失败!',
+          title: '提示',
+          showCancel: false,
+        });
         reject('Login Failed');
       },
       complete: () => {
+        wx.hideToast();
         wx.hideNavigationBarLoading();
       },
     });
@@ -142,9 +172,44 @@ export const switchTab = url =>
     });
   });
 
-export const showWarning = content =>
-  wx.showModal({
-    content,
-    title: '提示',
-    showCancel: false,
+export const showWarning = (content, showCancel = false) =>
+  new Promise((resolve, reject) => {
+    wx.showModal({
+      content,
+      title: '提示',
+      showCancel,
+      success: res => {
+        resolve(res);
+      },
+      fail: err => {
+        reject('Show Modal Error' + JSON.stringify(err));
+      },
+    });
+  });
+
+export const chooseLocation = () =>
+  new Promise((resolve, reject) => {
+    wx.chooseLocation({
+      success: res => {
+        resolve(res);
+      },
+      fail: err => {
+        reject('Choose Location Error' + JSON.stringify(err));
+      },
+    });
+  });
+
+export const chooseImage = () =>
+  new Promise((resolve, reject) => {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        resolve(res.tempFilePaths[0]);
+      },
+      fail: err => {
+        reject('Choose Image Error' + JSON.stringify(err));
+      },
+    });
   });

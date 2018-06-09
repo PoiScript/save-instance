164f1f35c34a0c4f2faf36ddf29678fc545baeae
PoiScript <poiscript@gmail.com>
2018-06-09 12:47:57 +0800

    feat(share): page styling

--- src/pages/share/index.vue
+++ src/pages/share/index.vue
@@ -1,12 +1,49 @@
 <template>
   <div class="container" v-if="video">
-    <video class="video" :src="video.video" :poster="video.thumbnail" controls></video>
+    <video class="video" :src="video.video" :poster="video.thumbnail" controls="false"></video>
+    <div class="weui-cells cell">
+      <div class="weui-cell weui-cell_access">
+        <div class="weui-cell_hd">
+          <img class="weui-cell_icon" src="/static/icons/description.png"/>
+        </div>
+        <div class="weui-cell__bd">视频名称</div>
+      </div>
+      <div class="cell-detail">
+        {{video.name || '未命名'}}
+      </div>
+      <div class="weui-cell weui-cell_access">
+        <div class="weui-cell_hd">
+          <img class="weui-cell_icon" src="/static/icons/period.png"/>
+        </div>
+        <div class="weui-cell__bd">生成时间</div>
+        <div class="weui-cell__ft">{{date}}</div>
+      </div>
+      <div class="weui-cell weui-cell_access" @click="downloadClick">
+        <div class="weui-cell_hd">
+          <img class="weui-cell_icon" src="/static/icons/download.jpg"/>
+        </div>
+        <div class="weui-cell__bd">下载视频</div>
+        <div class="weui-cell__ft weui-cell__ft_in-access"></div>
+      </div>
+      <a href="/pages/today/main" open-type="switchTab" hover-class="none" class="weui-cell weui-cell_access">
+        <div class="weui-cell_hd">
+          <img class="weui-cell_icon" src="/static/icons/video.png"/>
+        </div>
+        <div class="weui-cell__bd">返回首页</div>
+        <div class="weui-cell__ft weui-cell__ft_in-access"></div>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-import config from '../../config';
-import { request } from '../../util';
+import format from 'date-fns/format';
+
+import {
+  downloadFile,
+  request,
+  saveVideoToPhotosAlbum,
+} from '../../util';
 
 export default {
   data() {
@@ -15,22 +52,28 @@ export default {
     };
   },
 
-  onLoad() {
-    wx.showToast({
-      icon: 'loading',
-      title: '正在加载...',
-    });
-
-    request('video/share/' + this.$root.$mp.query.key)
-      .then(video => {
-        this.video = video;
-        this.video.thumbnail = config.api_url + this.video.video.replace(/\.mp4$/, '.jpg');
-        this.video.video = config.api_url + 'video/' + this.video.video;
-      })
-      .finally(() => wx.hideToast());
+  computed: {
+    date() {
+      return this.video ? format(this.video.created_at, 'M月D日 HH:mm') : '';
+    },
   },
 
-  methods: {},
+  onLoad() {
+    wx.showLoading({ title: '正在加载...' });
+
+    request('video/share/' + this.$root.$mp.query.share)
+      .then(video => (this.video = video))
+      .catch(err => console.log(err))
+      .finally(() => wx.hideLoading());
+  },
+
+  methods: {
+    downloadClick() {
+      downloadFile(this.video.video)
+        .then(path => saveVideoToPhotosAlbum(path))
+        .catch(err => console.log(err));
+    },
+  },
 };
 </script>
 
@@ -51,5 +94,17 @@ page {
 
 .video {
   width: 100%;
+}
+
+.cell {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.cell-detail {
+  padding: 0 15px 10px 15px;
+  font-size: 0.9em;
+  color: #555555;
 }
 </style>
